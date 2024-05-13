b9fbe8260004b505555de0bd0bb2d27cbeb4d403
gasensios <147517326+gasensios@users.noreply.github.com>
2024-05-13 22:35:52 +0200

    Actualizacion 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cabecera from "./componentes/estructura/Cabecera.jsx";
 import Navegacion from "./componentes/estructura/Navegacion.jsx";
 import Contenido from "./componentes/estructura/Contenido.jsx";
 import PiePagina from "./componentes/estructura/PiePagina.jsx";
+import './App.css';
 
 const App = () => {
   return (
